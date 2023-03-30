ddd87ddbc5f4edf987670cac88fb233a1ffb1f6c
Hudah <114993824+hudahansari@users.noreply.github.com>
2023-03-30 14:42:18 +0530

    Add files via upload

---
+++ PDF-to-CSV/main.py
@@ -0,0 +1,39 @@
+import pdfplumber
+import pandas as pd
+import sys
+
+
+def PDFToCSV(filesPath):
+  with pdfplumber.open(filesPath) as pdf:
+      writer = pd.ExcelWriter(f'{filesPath.replace(".pdf", "")}.xlsx', engine='xlsxwriter')
+      i = 1
+      table_settings = {
+        "vertical_strategy": "lines",
+        "horizontal_strategy": "text"
+      }
+      for page in pdf.pages:
+        # tables = page.find_tables()     
+        tables = page.find_tables(table_settings)     
+        if len(tables) > 0:
+          j = 0
+          for j in range(len(tables)):
+            tb = tables[j].extract()
+            df = pd.DataFrame(tb[1:], columns=tb[0])
+            sheetName = f'Sheet{i}' if len(tables) == 1 else f'Sheet{i}_{j + 1}'
+            df.to_excel(writer, sheet_name=sheetName, index=False)
+            j += 1
+        i += 1
+      writer.save()
+  
+  excel_file = writer
+  all_sheets = pd.read_excel(excel_file, sheet_name=None)
+  sheets = all_sheets.keys()
+
+  for sheet_name in sheets:
+      sheet = pd.read_excel(excel_file, sheet_name=sheet_name)
+      sheet.to_csv("%s.csv" % sheet_name, index=False)
+
+
+if __name__ == '__main__':
+  input_pdf = sys.argv[1]
+  PDFToCSV(input_pdf)
